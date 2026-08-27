1fe9c773a470a347b7111cae5b9d8b85eceba49e
seeba303 <noseba211@gmail.com>
2026-08-27 05:52:24 -0700

    Refactor file organizer with function

--- organizer.py
+++ organizer.py
@@ -4,34 +4,35 @@ os.makedirs("Images", exist_ok=True)
 os.makedirs("documents", exist_ok=True)
 os.makedirs("music", exist_ok=True)
 
+
+def move_file(filename, folder):
+    name, extension = os.path.splitext(filename)
+    destination = os.path.join(folder, filename)
+
+    counter = 1
+
+    while os.path.exists(destination):
+        destination = os.path.join(
+            folder,
+            name + "_" + str(counter) + extension
+        )
+        counter += 1
+
+    os.rename(filename, destination)
+
+
 for filename in os.listdir():
     name, extension = os.path.splitext(filename)
     extension = extension.lower()
 
     if extension == ".jpg" or extension == ".jpeg" or extension == ".png":
         print(filename, "is an image")
-        destination = "Images/" + filename
-
-        counter = 1
-
-        while os.path.exists(destination):
-            destination = "Images/" + name + "_" + str(counter) + extension
-            counter += 1
-
-        os.rename(filename, destination)
+        move_file(filename, "Images")
 
     elif extension == ".pdf":
         print(filename, "is a document")
-        os.rename(filename, "documents/" + filename)
+        move_file(filename, "documents")
 
     elif extension == ".mp3":
         print(filename, "is music")
-        destination = "music/" + filename
-
-        counter = 1
-
-        while os.path.exists(destination):
-            destination = "music/" + name + "_" + str(counter) + extension
-            counter += 1
-
-        os.rename(filename, destination)
+        move_file(filename, "music")
